f3bf570fd3318fb1ebf12e6d0dd4644aec34ceb4
Helder Nunes <helder.nunes46@gmail.com>
2020-05-23 12:39:37 +0100

    Network Manager Imageview Cache Infinite scroll

---
+++ BookStore_HelderNunes/Managers/NetworkManager.swift
@@ -0,0 +1,94 @@
+//
+//  NetworkManager.swift
+//  BookStore_HelderNunes
+//
+//  Created by Helder Nunes on 23/05/2020.
+//  Copyright © 2020 Helder Nunes. All rights reserved.
+//
+
+import UIKit
+
+class NetworkManager {
+    
+    static let shared = NetworkManager()
+    private let baseURL = "https://www.googleapis.com/books/v1/volumes"
+    
+    let cache = NSCache<NSString, UIImage>()
+    
+    private init() {}
+    
+    func getBooks(page: Int, completed: @escaping(Result<[Book], BSError>) -> Void) {
+        let endpoint = baseURL + "?q=ios&maxResults=20&startIndex=\(page)"
+        
+        guard let url = URL(string: endpoint) else {
+            completed(.failure(.invalidURL))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let _ = error {
+                completed(.failure(.unableToComplete))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completed(.failure(.invalidResponse))
+                return
+            }
+            
+            guard let data = data else {
+                completed(.failure(.invalidData))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let booksResponse = try decoder.decode(BookStoreResponse.self, from: data)
+                
+                if let books = booksResponse.items {
+                    completed(.success(books))
+                } else {
+                    completed(.success([]))
+                }
+                
+            } catch {
+                completed(.failure(.invalidData))
+            }
+        }
+        
+        task.resume()
+    }
+    
+    
+    func downloadImage(from urlString: String, completed: @escaping (UIImage?) -> Void) {
+        let cacheKey = NSString(string: urlString)
+        
+        if let image = cache.object(forKey: cacheKey) {
+            completed(image)
+            return
+        }
+        
+        guard let url = URL(string: urlString) else {
+            completed(nil)
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            
+            guard let self = self,
+                error == nil,
+                let response = response as? HTTPURLResponse, response.statusCode == 200,
+                let data = data,
+                let image = UIImage(data: data) else {
+                    completed(nil)
+                    return
+                }
+            
+            self.cache.setObject(image, forKey: cacheKey)
+            completed(image)
+        }
+        
+        task.resume()
+    }
+}
---
+++ BookStore_HelderNunes/Views/ImageViews/BSImageView.swift
@@ -0,0 +1,41 @@
+//
+//  BSImageView.swift
+//  BookStore_HelderNunes
+//
+//  Created by Helder Nunes on 23/05/2020.
+//  Copyright © 2020 Helder Nunes. All rights reserved.
+//
+
+import UIKit
+
+class BSImageView: UIImageView {
+
+    let cache = NetworkManager.shared.cache
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        layer.cornerRadius  = 10
+        clipsToBounds       = true
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    
+    func downloadImage(fromURL url: String) {
+        NetworkManager.shared.downloadImage(from: url) { [weak self] image in
+            guard let self = self else { return }
+            DispatchQueue.main.async { self.image = image }
+        }
+    }
+
+}
