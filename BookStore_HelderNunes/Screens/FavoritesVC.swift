0cc82197e9a217668e3ee39fab79a8290a6dc257
Helder Nunes <helder.nunes46@gmail.com>
2020-05-23 18:41:12 +0100

    Favorites list

--- BookStore_HelderNunes/Screens/FavoritesVC.swift
+++ BookStore_HelderNunes/Screens/FavoritesVC.swift
@@ -9,22 +9,97 @@
 import UIKit
 
 class FavoritesVC: UIViewController {
+    var booksCollectionView: UICollectionView!
+    var booksDataSource: [Book] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .red
+        
+        configureViewController()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        getBooks()
     }
-    */
+    
+    
+    //MARK: UI
+    
+    
+    private func configureViewController() {
+        view.backgroundColor = .systemBackground
+        
+        configureBooksCollectionView()
+    }
+    
+    
+    
+    private func configureBooksCollectionView() {
+        booksCollectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createTwoColumnFlowLayout(in: view))
+        view.addSubview(booksCollectionView)
+        booksCollectionView.delegate = self
+        booksCollectionView.dataSource = self
+        booksCollectionView.backgroundColor = .systemBackground
+        booksCollectionView.register(BookCollectionViewCell.self, forCellWithReuseIdentifier: BookCollectionViewCell.reuseId)
+    }
+    
+    
+    //MARK: Local Storage
+    
+    func getBooks() {
+        
+        PersistenceManager.retrieveFavorites { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(let books):
+                self.updateUI(with: books)
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    let alert = UIAlertController(title: "Something went wrong", message: error.rawValue, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+    
+    func updateUI(with books: [Book]) {
+        booksDataSource = books
+        
+        DispatchQueue.main.async {
+            self.booksCollectionView.reloadData()
+        }
+    }
 
+}
+
+
+extension FavoritesVC: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let bookDetailVC = BookDetailVC()
+        bookDetailVC.book = booksDataSource[indexPath.row]
+        navigationController?.pushViewController(bookDetailVC, animated: true)
+    }
+    
+}
+
+
+extension FavoritesVC: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return booksDataSource.count
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: BookCollectionViewCell.reuseId, for: indexPath) as! BookCollectionViewCell
+        let book = booksDataSource[indexPath.row]
+        cell.set(book: book)
+        return cell
+    }
 }
